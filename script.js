42c7292bfdc1ec444852dd360e0621296eef4f70
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-10-15 19:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-	
-  arr.sort((a,b)=>a-b);
-	let sum=0;
-	let total=0
-	// let sum=arr[0]+arr[1];
-	for(i=0;i<arr.length;i++){
-		sum+=arr[i];
-		if(i>0) total+=sum;
-	}
-	return total;
-}
+function mincost(arr) {
+  let total = 0;
+  arr.sort((a, b) => a - b);
 
+  while (arr.length > 1) {
+    // Take two smallest
+    let sum = arr[0] + arr[1];
+    total += sum;
+
+    // Remove them
+    arr.splice(0, 2);
+
+    // Insert new rope length and sort again
+    arr.push(sum);
+    arr.sort((a, b) => a - b);
+  }
+
+  return total;
+}
 module.exports=mincost;
