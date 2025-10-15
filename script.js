d958d699bc96e3767e8dab98d4fcfdf7a50f4198
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-10-15 18:59:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	
+  arr.sort((a,b)=>a-b);
+	let sum=0;
+	let total=0
+	// let sum=arr[0]+arr[1];
+	for(i=0;i<arr.length;i++){
+		sum+=arr[i];
+		if(i>0) total+=sum;
+	}
+	return total;
 }
 
 module.exports=mincost;
